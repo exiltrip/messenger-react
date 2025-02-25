f72a8ff2b50d950656a9a7f7e043ec65c4c1f4ae
Developer <hobu4ok.5555@gmail.com>
2025-02-25 18:21:00 +0300

    fix: chat read signals fix

--- src/pages/chat/ui/ChatWindow.tsx
+++ src/pages/chat/ui/ChatWindow.tsx
@@ -48,7 +48,7 @@ const ChatWindow = () => {
                     <div key={index} className={`w-full p-2 grow-0 flex flex-col `}>
                         <div className={`min-w-min max-w-max bg-slate-200 flex py-1 px-3 rounded-2xl ${message.peer_id !== clientPeerId && "mr-5 md:mr-20"} ${message.peer_id == clientPeerId && "self-end"} ${message.peer_id == clientPeerId ? "rounded-tr-none" : "rounded-tl-none"}`}>{message.message}
                             <i className="text-nowrap text-xs text-slate-500 self-end pl-1">{formatDate(message.timestamp)}</i>
-                            {message.peer_id == clientPeerId && message.read ? <DoneAllIcon className="!size-5 text-slate-600 self-end pl-1"/>: <DoneIcon className="!size-5 text-slate-600 self-end pl-1"/>}
+                            {message.peer_id === clientPeerId ? message.read ? <DoneAllIcon className="!size-5 text-slate-600 self-end pl-1"/>: <DoneIcon className="!size-5 text-slate-600 self-end pl-1"/> : ""}
                         </div>
 
                     </div>
